06f4b17f8471496a0187226827d3c9a1a8cb0ffd
Yoel Monzon <yoelfme@hotmail.com>
2015-10-08 13:08:53 -0600

    [add] connect to database

---
+++ models/index.js
@@ -0,0 +1,10 @@
+var config = require('./../config');
+var mongoose = require('mongoose');
+
+mongoose.connect(config.db, function(err, res) {
+    if(err) {
+        console.log('ERROR connecting to: ' + config.db + '. ' + err);
+    } else {
+        console.log('Succeeded connected to: ' + config.db)
+    }
+});
